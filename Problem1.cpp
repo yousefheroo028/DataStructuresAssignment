b3a1ecb11ba4620c8bd40265c49fa4ce6ac24dfd
Youssef Hassan <yousefheroo028@gmail.com>
2025-03-26 00:54:39 +0200

    Update Problem1.cpp
    remove_guest(name) was added.
    IftarManager(int size) was added.
    Some improvements.

--- Problem1.cpp
+++ Problem1.cpp
@@ -5,132 +5,137 @@ using namespace std;
 class Guest
 {
 protected:
-    string name;
-    string contact;
-    string iftar_date;
+	string name;
+	string contact;
+	string iftar_date;
 
 public:
-    Guest()
-    {
-        name = "";
-        contact = "";
-        iftar_date = "";
-    }
+	Guest() : name(""), contact(""), iftar_date("") {}
 
-    Guest(const string& name, const string& contact, const string& iftar_date)
-    {
-        this->name = name;
-        this->contact = contact;
-        this->iftar_date = iftar_date;
-    }
+	Guest(const string& name, const string& contact, const string& iftar_date) : name(name), contact(contact), iftar_date(iftar_date) {}
 
-    void display_guest() const
-    {
-        cout << "Guest: " << this->name << ", " << this->contact << ", Iftar Date: " << this->iftar_date << endl;
-    }
+	void display_guest() const
+	{
+		cout << "Guest: " << this->name << ", " << this->contact << ", Iftar Date: " << this->iftar_date << endl;
+	}
 
-    void update_invitation(const string& new_date)
-    {
-        this->iftar_date = new_date;
-    }
+	void update_invitation(const string& new_date)
+	{
+		this->iftar_date = new_date;
+	}
 
-    friend class IftarManager;
+	friend class IftarManager;
 };
 
 class IftarManager
 {
 private:
-    Guest* guest_list;
-    int numGuests;
+	Guest* guest_list;
+	int no_guests;
 
 public:
-    IftarManager()
-    {
-        guest_list = nullptr;
-        numGuests = 0;
-    }
+	IftarManager()
+	{
+		guest_list = nullptr;
+		no_guests = 0;
+	}
 
-    void add_guest(const Guest& guest)
-    {
-        Guest* temp = new Guest[numGuests + 1];
-        for (int i = 0; i < numGuests; i++)
-        {
-            if (guest_list != nullptr) temp[i] = guest_list[i];
-        }
-        temp[numGuests] = guest;
-        guest_list = temp;
-        numGuests++;
-    }
+	IftarManager(const int& no_guests) : no_guests(no_guests) {
+		guest_list = new Guest[no_guests];
+	}
 
-    void display_all_guests() const
-    {
-        for (int i = 0; i < numGuests; i++)
-        {
-            guest_list[i].display_guest();
-        }
-    }
+	void add_guest(const Guest& guest)
+	{
+		Guest* temp = new Guest[no_guests + 1];
+		for (int i = 0; i < no_guests; i++)
+		{
+			temp[i] = guest_list[i];
+		}
+		temp[no_guests] = guest;
+		guest_list = temp;
+		no_guests++;
+	}
 
-    void update_guest_invitation(const string& name, const string& new_date) const
-    {
-        cout << "\nUpdating invitation for " << name << "..." << "\n\n";
-        for (int i = 0; i < numGuests; i++)
-        {
-            if (name == guest_list[i].name)
-            {
-                guest_list[i].update_invitation(new_date);
-                break;
-            }
-        }
-        cout << name << " is not found!\n\n";
-    }
+	void remove_guest(const string& name) {
+		cout << "\nRemoving " << name << "..." << "\n";
+		Guest* temp = new Guest[no_guests - 1];
+		for (int i = 0, j = 0; i < no_guests; i++) {
+			if (guest_list[i].name != name) temp[j++] = guest_list[i];
+		}
+		guest_list = temp;
+		no_guests--;
+	}
 
-    void send_reminder(const string& date) const
-    {
-        cout << "\nSending reminders...\n\n";
-        for (int i = 0; i < numGuests; i++)
-        {
-            cout << "Reminder sent to " << guest_list[i].name << ": Your Iftar invitation is on " << date << '\n';
-        }
-        cout << "\nMay your Iftar gatherings be full of warmth and blessings!" << endl;
-    }
+	void display_all_guests() const
+	{
+		for (int i = 0; i < no_guests; i++)
+		{
+			guest_list[i].display_guest();
+		}
+	}
 
-    void sort_guest_list() const
-    {
-        cout << "\nSorting guest list...\n\n";
-        for (int i = 0; i < numGuests; i++)
-        {
-            for (int j = 0; j < numGuests - i - 1; j++)
-            {
-                if (guest_list[j].name > guest_list[j + 1].name)
-                {
-                    const Guest temp = guest_list[j];
-                    guest_list[j] = guest_list[j + 1];
-                    guest_list[j + 1] = temp;
-                }
-            }
-        }
-    }
+	void update_guest_invitation(const string& name, const string& new_date) const
+	{
+		cout << "\nUpdating invitation for " << name << "..." << "\n\n";
+		for (int i = 0; i < no_guests; i++)
+		{
+			if (name == guest_list[i].name)
+			{
+				guest_list[i].update_invitation(new_date);
+				break;
+			}
+		}
+		cout << name << " is not found!\n\n";
+	}
 
-    ~IftarManager()
-    {
-        delete[] guest_list;
-    }
+	void send_reminder(const string& date) const
+	{
+		cout << "\nSending reminders...\n\n";
+		for (int i = 0; i < no_guests; i++)
+		{
+			cout << "Reminder sent to " << guest_list[i].name << ": Your Iftar invitation is on " << date << '\n';
+		}
+		cout << "\nMay your Iftar gatherings be full of warmth and blessings!" << endl;
+	}
+
+	void sort_guest_list() const
+	{
+		cout << "\nSorting guest list...\n\n";
+		for (int i = 0; i < no_guests; i++)
+		{
+			for (int j = 0; j < no_guests - i - 1; j++)
+			{
+				if (guest_list[j].name > guest_list[j + 1].name)
+				{
+					const Guest temp = guest_list[j];
+					guest_list[j] = guest_list[j + 1];
+					guest_list[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	~IftarManager()
+	{
+		delete[] guest_list;
+	}
 };
 
 int main()
 {
-    IftarManager iftarManager = IftarManager();
-    const Guest guest1 = Guest("Zainab", "zainab@example.com", "2025-03-20");
-    const Guest guest2 = Guest("Aisha", "aisha@example.com", "2025-03-15");
-    const Guest guest3 = Guest("Omar", "omar@example.com", "2025-03-18");
-    iftarManager.add_guest(guest1);
-    iftarManager.add_guest(guest2);
-    iftarManager.add_guest(guest3);
-    iftarManager.display_all_guests();
-    iftarManager.update_guest_invitation("Omar", "2025-03-15");
-    iftarManager.display_all_guests();
-    iftarManager.sort_guest_list();
-    iftarManager.display_all_guests();
-    iftarManager.send_reminder("2025-03-18");
-    return 0;
+	IftarManager iftarManager = IftarManager();
+	const Guest guest1 = Guest("Zainab", "zainab@example.com", "2025-03-20");
+	const Guest guest2 = Guest("Aisha", "aisha@example.com", "2025-03-15");
+	const Guest guest3 = Guest("Omar", "omar@example.com", "2025-03-18");
+	iftarManager.add_guest(guest1);
+	iftarManager.add_guest(guest2);
+	iftarManager.add_guest(guest3);
+	iftarManager.display_all_guests();
+	iftarManager.remove_guest("Zainab");
+	iftarManager.update_guest_invitation("Omar", "2025-03-15");
+	iftarManager.display_all_guests();
+	iftarManager.sort_guest_list();
+	iftarManager.display_all_guests();
+	iftarManager.send_reminder("2025-03-18");
+	return 0;
 }
