ee91b597b6e77b701f130070a480ddeb3c0c1fda
Youssef Hassan <yousefheroo028@gmail.com>
2025-05-05 09:45:52 +0300

    Adding Validation of the new URL before Visiting it.

--- Assignment 2/Problems/Problem1.cpp
+++ Assignment 2/Problems/Problem1.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include "..\Headers\Stack.h"
+#include <regex>
+#include <thread>
+#include <string>
 
 using namespace std;
 
+bool isValidURL(const string& url)
+{
+    const regex pattern(R"(^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$)");
+    return regex_match(url, pattern);
+}
+
 Stack forwardStack, backStack;
+
 string currentURL;
 
 string visit(const string& URL)
@@ -21,6 +31,16 @@ string goBack()
     return currentURL;
 }
 
+void loading()
+{
+    this_thread::sleep_for(500ms);
+    cout << '.';
+    this_thread::sleep_for(1000ms);
+    cout << '.';
+    this_thread::sleep_for(1000ms);
+    cout << '.' << endl;
+}
+
 string goForward()
 {
     backStack.push(forwardStack.pop());
@@ -30,10 +50,11 @@ string goForward()
 
 void menu()
 {
+    cout << "Salam, ";
     char operation;
     do
     {
-        cout << "Salam, choose the operation you want:\n"
+        cout << "choose the operation you want:\n"
             "1. Visit a URL\n"
             "2. Go back\n"
             "3. Go forward\n"
@@ -45,34 +66,57 @@ void menu()
         case '1':
             cout << "Enter the URL to be visited: ";
             cin >> URL;
-            cout << "Going to \"" << URL << "\"...\n";
-            visit(URL);
+            cout << "Validating \"" << URL << "\"";
+            loading();
+            if (isValidURL(URL))
+            {
+                cout << "\"" << URL << "\" is valid\n";
+                cout << "Going to \"" << URL << "\"";
+                loading();
+                visit(URL);
+                cout << "\"" << URL << "\"" << " is reached\n";
+            }
+            else
+            {
+                cout << "\"" << URL << "\" is not valid\n";
+            }
+            cout << "Welcome back, ";
             break;
         case '2':
             if (backStack.size() < 2)
             {
-                cout << "Going back is impossible...\n";
+                cout << "Going back is impossible\n";
             }
             else
             {
-                cout << "Going back to " << goBack() << endl;
+                URL = goBack();
+                cout << "Going back to \"" << URL << "\"";
+                loading();
+                cout << "\"" << URL << "\"" << " is reached\n";
             }
+            cout << "Welcome back, ";
             break;
         case '3':
             if (forwardStack.size() < 1)
             {
-                cout << "Going Forward is impossible...\n";
+                cout << "Going Forward is impossible\n";
             }
             else
             {
-                cout << "Going Forward to " << goForward() << endl;
+                URL = goForward();
+                cout << "Going Forward to \"" << URL << "\"";
+                loading();
+                cout << "\"" << URL << "\"" << " is reached\n";
             }
+            cout << "Welcome back, ";
             break;
         case '4':
-            cout << "Quiting...";
+            cout << "Quiting";
+            loading();
             break;
         default:
             cout << "Invalid Choice\n";
+            cout << "Welcome back, ";
             break;
         }
     }
